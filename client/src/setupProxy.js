2cde633731fbeffdfa6b10cf913463ee8415a7d3
stkang9409 <58027521+stkang9409@users.noreply.github.com>
2022-06-05 01:32:57 +0900

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const config = require("./config/key")
 module.exports = function (app) {
-
+    console.log("가자~~", config.SERVER);
     app.use(
         '/api',
         createProxyMiddleware({
