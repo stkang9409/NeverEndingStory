f364a9f02a35835d471f1d045bfefb6f31aac0f5
stkang9409 <58027521+stkang9409@users.noreply.github.com>
2022-06-06 11:24:29 +0900

    Update Config.js

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,5 @@
 export const USER_SERVER = '/api/users';
 // ip address ( please be careful... )
 // export const LOCAL_HOST = "15.164.48.192";
-export const LOCAL_HOST = "localhost";
+export const LOCAL_HOST = "www.iovar2.com";
 
