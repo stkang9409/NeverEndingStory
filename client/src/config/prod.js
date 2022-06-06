64c937d1c65228b6c78d64bbacaf14fd4d094a68
stkang9409 <58027521+stkang9409@users.noreply.github.com>
2022-06-06 10:39:45 +0900

    Update prod.js

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     STORAGE: 'https://iovar.s3.ap-northeast-2.amazonaws.com',
     SERVER: 'https://api.iovar2.com',
-    CLIENT: 'https://www.iovar.co.kr',
+    CLIENT: 'https://www.iovar2.com',
     SOCKET: 'http://15.164.164.235:5000',
     KAKAO_KEY: '31ba9e7c5c25cdc29ae62436da6f8733'
 }
